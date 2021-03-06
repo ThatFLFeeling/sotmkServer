8ae92dcdcfe501f6e2514d45a6ad6e310011a93d
ThatFLFeeling <77360022+ThatFLFeeling@users.noreply.github.com>
2021-03-06 17:27:00 -0500

    Moved file writing into while loop

--- sotmkRFIDScan.py
+++ sotmkRFIDScan.py
@@ -4,24 +4,24 @@ sys.path.append('/home/pi/MFRC522-python')
 from mfrc522 import SimpleMFRC522
 import threading
 
+#File writing related variables
+FILENAME = 'time.txt' #File to be overwritten with current timestamp
+
 def scan():
         while(True):
             id, _ = reader.read()
             print(id)
             currentScan = id
+            now = round(time.time())
+
+            #When an RFID is read, write timestamp to file
+            if (currentScan > 0):
+                timestamp_file = open(FILENAME, 'w')
+                timestamp_file.write(str(now))
+                timestamp_file.close()
 
 #RFID related variables
 reader = SimpleMFRC522()
 currentScan = 0
 scanThread = threading.Thread(target = scan())
 scanThread.start()
-
-#File writing related variables
-FILENAME = 'time.txt' #File to be overwritten with current timestamp
-now = round(time.time())
-
-#When an RFID is read, write timestamp to file
-if (currentScan > 0):
-    timestamp_file = open(FILENAME, 'w')
-    timestamp_file.write(str(now))
-    timestamp_file.close()
